d368410581b90562c5afc391ca5ab7a378fb463f
rbekbatyrov <69893694+rbekbatyrov@users.noreply.github.com>
2021-11-14 00:29:46 +0600

    Update helloworld.go

--- helloworld.go
+++ helloworld.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"reflect"
 	"strings"
 )
 
@@ -12,4 +13,9 @@ func main() {
 	fmt.Println(strings.Title("hello \n\tworld!!!"))
 	fmt.Println("\"\"")
 	fmt.Println("\\")
+	fmt.Println('B') //rune
+	fmt.Println(reflect.TypeOf(42))
+	fmt.Println(reflect.TypeOf(3.14))
+	fmt.Println(reflect.TypeOf(true))
+	fmt.Println(reflect.TypeOf("helloworld!!!"))
 }
