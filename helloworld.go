c5d8c3906f8235f20a237fecc420d8c7d288881d
rbekbatyrov <69893694+rbekbatyrov@users.noreply.github.com>
2021-11-14 00:44:34 +0600

    Update helloworld.go

--- helloworld.go
+++ helloworld.go
@@ -18,4 +18,13 @@ func main() {
 	fmt.Println(reflect.TypeOf(3.14))
 	fmt.Println(reflect.TypeOf(true))
 	fmt.Println(reflect.TypeOf("helloworld!!!"))
+	var variable_int int
+	variable_int = 1
+	fmt.Println(variable_int)
+	var variable_float_1, variable_float_2 float64 = 1.2, 3.4
+	fmt.Println(variable_float_1, variable_float_2)
+	var variable_hello, variable_world string
+	variable_hello = "hello"
+	variable_world = "world"
+	fmt.Println(strings.Title(variable_hello), strings.Title(variable_world), "!!!")
 }
